6ad88240c8f8db946b3f71cb59c88d7b429facee
David Weinstein <dwein9@gmail.com>
2017-06-14 12:35:38 -0400

    can return values from form

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ $(document).ready(() => {
    onClick()
  })
 
- function onClick() {
-   $("#submit").click(() => {
-     $("#show-form").hide()
-     $("#results").show()
-     let name = (document.getElementById('name').value)
-     let age = (document.getElementById('age').value)
 
-     $("#results").append(`<div><h2>${name}</h2></div>`)
-   })
- }
+  function onClick() {
+     $("#submit").click(() => {
+       $("#show-form").hide()
+       $("#results").show()
+       let name = (document.getElementById('name').value)
+       let age = (document.getElementById('age').value)
+       let sex = $('input[name=gender]:checked').val()
+       let conditions = $('input[name="conditions"]:checked').serialize().split('conditions=').join('').replace(/&/g, " ")
 
-  // function getInfo() {
-  // }
-  //
-  // function Results (name, age){
-  //   this.name = name
-  //   this.age = age
-  // }
+       $("#results").append(`<div>
+         <h2>${name}</h2> <br/>
+         <p>${age}</p>
+         <p>${sex}</p>
+         <p>${conditions}</p>
+       </div>`)
+     })
+   }
